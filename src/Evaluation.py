0498503eedab7e774d6e37c6d6af45bb96ec3fcf
Jan Gerling <jan.gerling@posteo.de>
2019-06-15 17:57:37 +0200

    added plotting of coherence and perplexity scores and refactorings

--- src/Evaluation.py
+++ src/Evaluation.py
@@ -1,7 +1,16 @@
-import gensim
-from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel, ldamodel
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plotter
+
+
+def evaluateModels(topicModels, dictionary, corpus, features, topicCounts):
+    coherenceScores = []
+    perplexityScores = []
+    for (amountTopics, topicModel) in topicModels:
+        perplexityScore, coherenceScore = evaluateModel(topicModel, dictionary, corpus, features, amountTopics)
+        coherenceScores.append(coherenceScore)
+        perplexityScores.append(perplexityScore)
+
+    plot(topicCounts, coherenceScores, perplexityScores)
 
 
 def evaluateModel(topicModel, dict, bow, features, amountTopics):
@@ -21,4 +30,18 @@ def computePerplexityScore(topicModel, bow):
 def computeCoherenceScores(topicModel, dictionary, features):
     coherenceModel = CoherenceModel(model=topicModel, texts=features, dictionary=dictionary, coherence='c_v')
     return coherenceModel.get_coherence()
+
+
+def plot(topicCounts, coherenceScores, perplexityScores):
+    plotter.plot(topicCounts, coherenceScores)
+    plotter.xlabel("# Topics")
+    plotter.ylabel("Coherence Score")
+    plotter.legend("coherence scores", loc='best')
+    plotter.show()
+
+    plotter.plot(topicCounts, perplexityScores)
+    plotter.xlabel("# Topics")
+    plotter.ylabel("Perplexity Score")
+    plotter.legend("perplexity scores", loc='best')
+    plotter.show()
 
--- src/TopicModeler.py
+++ src/TopicModeler.py
@@ -1,17 +1,15 @@
 # Gensim
-import gensim
-from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel, ldamodel
 # Custom
 from CorpusBuilder import buildCorpus
 from Preprocessor import preProcess
-from Evaluation import evaluateModel
+from Evaluation import evaluateModels
 # Other
 from typing import List, Any
 import math
 
 
-def modelTopics(featureLists: List[List[str]]) -> List[List[str]]:
+def modelTopics(featureLists: List[List[str]], topicMin = 2, topicLimit = 256) -> List[List[str]]:
     print('\nRaw input:', featureLists)
 
     processedFeatures = preProcess(featureLists)
@@ -20,13 +18,13 @@ def modelTopics(featureLists: List[List[str]]) -> List[List[str]]:
     dictionary, corpus = buildCorpus(processedFeatures)
     print('\nCorpus (Dictionary, Bag of Words):', corpus)
 
-    topicModels = generateTopicModels(dictionary, corpus, 256)
-    for (amountTopics, topicModel) in topicModels:
-        evaluateModel(topicModel, dictionary, corpus, processedFeatures, amountTopics)
+    topicCounts = [1 << exponent for exponent in range(int(math.log(topicMin, 2)), int(math.log(topicLimit, 2)) + 1)]
+    topicModels = generateTopicModels(dictionary, corpus, topicCounts)
+
+    evaluateModels(topicModels, dictionary, corpus, processedFeatures, topicCounts)
 
 
-def generateTopicModels( dictionary, bow, limit, start = 2):
-    topicCounts = [1<< x for x in range(int(math.log(start, 2)), int(math.log(limit, 2)) + 1)]
+def generateTopicModels( dictionary, bow, topicCounts):
     models = []
     for amountTopics in topicCounts:
         ldaModel = ldamodel.LdaModel(bow, amountTopics, dictionary, passes=20, alpha='auto', per_word_topics=True)
@@ -42,4 +40,4 @@ testFeatureLists = [testFeatureList01, testFeatureList02]
 
 # execute in parallel, otherwise it taks to long
 if __name__ == '__main__':
-    modelTopics(testFeatureLists).runInParallel(numProcesses=4, numThreads=8)
+    modelTopics(testFeatureLists, 2, 256).runInParallel(numProcesses=4, numThreads=8)
